65e6a3e6ce322907ddafe090290c960f104abc11
Cliff Chipman <61033936+cchipman21804@users.noreply.github.com>
2021-08-28 17:54:28 -0400

    Add files via upload

--- collatzConjecture.py
+++ collatzConjecture.py
@@ -22,13 +22,17 @@ while not validInput:
 #
 # Set iterations to 0
 i = 0
+maxValue = 0
 while x > 1:
     if x % 2 == 0:
         x /= 2
     else:
         x = x*3 + 1
+    if x > maxValue:
+        maxValue = x
     i += 1
     print(int(x))
-print(f"Iterations: {i}")
+print(f"Iterations   : {i}")
+print(f"Maximum Value: {int(maxValue):,.0f}")
 endedAt = dt.datetime.now()
 print(f"Runtime: {(endedAt - startedAt).seconds} seconds")
